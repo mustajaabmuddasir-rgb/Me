56a60df02ac1003d02899321123be658d2f90eea
mustajaabmuddasir-rgb <mustajaabmuddasir@gmail.com>
2026-03-25 20:15:37 +0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
 from sklearn.model_selection import train_test_split
